683632f548d7dc8d0f625335968c344600086023
David Dumke <david.dumke@gmail.com>
2023-12-07 14:47:04 -0300

    Update __init__.py

--- tap_mssql/__init__.py
+++ tap_mssql/__init__.py
@@ -89,6 +89,8 @@ def default_singer_decimal():
 def schema_for_column(c, config):
     """Returns the Schema object for the given Column."""
     data_type = c.data_type.lower()
+    
+LOGGER.info("DAAAAAAAAAAAAAAAAVID - Campo do tipo %s", data_type)
 
     inclusion = "available"
 
