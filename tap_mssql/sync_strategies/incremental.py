91c00154723f9b90605dd1dd64da1ad36eb3a57e
David Renato Dumke <157407747+david-veda@users.noreply.github.com>
2024-04-07 14:38:24 -0300

    Update incremental.py

--- tap_mssql/sync_strategies/incremental.py
+++ tap_mssql/sync_strategies/incremental.py
@@ -56,10 +56,12 @@ def sync_table(mssql_conn, config, catalog_entry, state, columns):
                 if catalog_entry.schema.properties[replication_key_metadata].format == "date-time":  
                     # comentado pelo David
                     # replication_key_value = pendulum.parse(replication_key_value)
+                    LOGGER.info("Não é mais transformada a data")
 
                 select_sql += ' WHERE "{}" >= %(replication_key_value)s ORDER BY "{}" ASC'.format(
                     replication_key_metadata, replication_key_metadata
                 )
+                
                 LOGGER.info(f"Cláusula where gerada com stamp: {select_sql}")
 
                 params["replication_key_value"] = replication_key_value
