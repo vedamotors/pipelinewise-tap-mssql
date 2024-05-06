b8e0fdaa6d0cea0877e2508e2e7f07a424042206
Dev Vedamotors <servicos@vedamotors.com.br>
2024-05-06 14:22:31 -0300

    Update __init__.py

--- tap_mssql/__init__.py
+++ tap_mssql/__init__.py
@@ -165,6 +165,7 @@ def schema_for_column(c, config):
 
     elif data_type in IMAGE_TYPES:
         result.type = ["null", "string"]
+        result.contentEncoding = "base64"
         # When length is -1 it is a long column type
         # https://docs.microsoft.com/en-us/sql/relational-databases/system-information-schema-views/columns-transact-sql?view=sql-server-ver15
         # -1 is not valid JSON schema
