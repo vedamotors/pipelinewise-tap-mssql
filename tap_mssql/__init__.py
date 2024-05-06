1c1758c4f686eee4682a395af884ec570b609454
Dev Vedamotors <servicos@vedamotors.com.br>
2024-05-06 15:24:22 -0300

    Update __init__.py

--- tap_mssql/__init__.py
+++ tap_mssql/__init__.py
@@ -164,17 +164,15 @@ def schema_for_column(c, config):
         result.type = ["null", "object"]
 
     elif data_type in IMAGE_TYPES:
-        result.format = "image"
         result.type = ["null", "string"]
-        result.contentEncoding = "base64"
+        result.format = "image"
+        # result.contentEncoding = "base64"
         # When length is -1 it is a long column type
         # https://docs.microsoft.com/en-us/sql/relational-databases/system-information-schema-views/columns-transact-sql?view=sql-server-ver15
         # -1 is not valid JSON schema
         # https://json-schema.org/understanding-json-schema/reference/string.html#length
-        if c.character_maximum_length != -1:
-            result.maxLength = c.character_maximum_length
-            
-        # result.type = ["null", "bytes-like"]
+        #if c.character_maximum_length != -1:
+        #    result.maxLength = c.character_maximum_length
 
     else:
         result = Schema(
