6895d635acf288e92d706e3c7aa125b2e11731ba
Dev Vedamotors <servicos@vedamotors.com.br>
2024-05-06 15:49:18 -0300

    Update incremental.py

--- tap_mssql/sync_strategies/incremental.py
+++ tap_mssql/sync_strategies/incremental.py
@@ -56,7 +56,7 @@ def sync_table(mssql_conn, config, catalog_entry, state, columns):
                 if catalog_entry.schema.properties[replication_key_metadata].format == "date-time":  
                     # comentado pelo David
                     # replication_key_value = pendulum.parse(replication_key_value)
-                    LOGGER.info("Não é mais transformada a data")
+                    # LOGGER.info("Não é mais transformada a data")
 
                 select_sql += ' WHERE "{}" >= %(replication_key_value)s ORDER BY "{}" ASC'.format(
                     replication_key_metadata, replication_key_metadata
